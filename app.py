28fdf61d6043bcdeb50e518aa22a7228b563f408
Nikhil Menon <nike2447@gmail.com>
2023-04-23 14:33:51 +0530

    Final commit

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import *
+from tkinter import messagebox
 
 import sqlite3
 from sqlite3 import Error
 
+
 class Product:
     db_name = 'database.db'
+    total = 0
 
     def create_connection(self):
         conn = None
         try:
             conn = sqlite3.connect(self.db_name)
             cursor = conn.cursor()
-            cursor.execute('CREATE TABLE IF NOT EXISTS product (Name TEXT, Price REAL)')
+            cursor.execute(
+                'CREATE TABLE IF NOT EXISTS product (Name TEXT, Price REAL)')
             conn.commit()
             print(sqlite3.version)
         except Error as e:
@@ -21,45 +25,51 @@ class Product:
         finally:
             if conn:
                 conn.close()
-                
+
 #     def create_table(self):
 #         with sqlite3.connect(self.db_name) as conn:
 #             cursor = conn.cursor()
 #             cursor.execute('CREATE TABLE IF NOT EXISTS product (Name TEXT, Price REAL)')
 #             conn.commit()
 
-
     def __init__(self, window):
         self.wind = window
         self.wind.title('Products Application')
 
-        frame = LabelFrame(self.wind, text = 'Register new Product')
-        frame.grid(row = 0, column = 0, columnspan = 3, pady = 20)
+        frame = LabelFrame(self.wind, text='Register new Product')
+        frame.grid(row=0, column=0, columnspan=3, pady=20)
 
-        Label(frame, text = 'Name: ').grid(row = 1, column = 0)
+        Label(frame, text='Name: ').grid(row=1, column=0)
         self.name = Entry(frame)
         self.name.focus()
-        self.name.grid(row = 1, column = 1)
+        self.name.grid(row=1, column=1)
 
-        Label(frame, text = 'Price: ').grid(row = 2, column = 0)
+        Label(frame, text='Price: ').grid(row=2, column=0)
         self.price = Entry(frame)
-        self.price.grid(row = 2, column = 1)
+        self.price.grid(row=2, column=1)
 
-        ttk.Button(frame, text = 'Save Product', command = self.add_product).grid(row = 3, columnspan = 2, sticky = W + E)
+        ttk.Button(frame, text='Save Product', command=self.add_product).grid(
+            row=3, columnspan=2, sticky=W + E)
 
-        self.message = Label(text = '', fg = 'red')
-        self.message.grid(row = 3, column = 0, columnspan = 2, sticky = W + E)
+        ttk.Button(frame, text='Clear Database', command=self.clear_database).grid(
+            row=4, columnspan=2, sticky=W + E)
 
-        self.tree = ttk.Treeview(height = 10, columns = 2)
-        self.tree.grid(row = 4, column = 0, columnspan = 2)
-        self.tree.heading('#0', text = 'Name', anchor = CENTER)
-        self.tree.heading('#1', text = 'Price', anchor = CENTER)
-        
+        self.message = Label(text='', fg='red')
+        self.message.grid(row=3, column=0, columnspan=2, sticky=W + E)
+
+        self.tree = ttk.Treeview(height=10, columns=2)
+        self.tree.grid(row=4, column=0, columnspan=2)
+        self.tree.heading('#0', text='Name', anchor=CENTER)
+        self.tree.heading('#1', text='Price', anchor=CENTER)
+
+        self.total_price_label = Label(text='Total Price: $0.00')
+        self.total_price_label.grid(row=5, column=0, columnspan=2, sticky=W+E)
+
         self.create_connection()
 
         self.get_products()
 
-    def run_query(self, query, parameters = ()):
+    def run_query(self, query, parameters=()):
         with sqlite3.connect(self.db_name) as conn:
             cursor = conn.cursor()
             result = cursor.execute(query, parameters)
@@ -73,20 +83,35 @@ class Product:
         query = 'SELECT * FROM product ORDER BY name DESC'
         db_rows = self.run_query(query)
         for row in db_rows:
-            self.tree.insert('', 0, text = row[0], values = row[1])
+            self.tree.insert('', 0, text=row[0], values=row[1])
 
     def add_product(self):
         query = 'INSERT INTO product VALUES(?, ?)'
-        parameters =  (self.name.get(), self.price.get())
+        parameters = (self.name.get(), self.price.get())
+        self.total += float(self.price.get())
         self.run_query(query, parameters)
-        self.message['text'] = 'Product {} added Successfully'.format(self.name.get())
+        self.message['text'] = 'Product {} added Successfully'.format(
+            self.name.get())
         self.name.delete(0, END)
         self.price.delete(0, END)
         self.get_products()
 
+        self.total_price_label['text'] = 'Total Price: ${:.2f}'.format(
+            self.total)
+
+    def clear_database(self):
+        if messagebox.askyesno('Confirmation', 'Are you sure you want to clear the database?'):
+            query = 'DELETE FROM product'
+            self.run_query(query)
+            self.get_products()
+            self.total = 0
+            self.total_price_label = Label(text='Total Price: $0.00')
+            self.total_price_label.grid(
+                row=5, column=0, columnspan=2, sticky=W+E)
+
 
 if __name__ == '__main__':
-#     Product.create_connection("database.db")
+    #     Product.create_connection("database.db")
     window = tk.Tk()
     application = Product(window)
     window.mainloop()
